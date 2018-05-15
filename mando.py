fd865eb6d7f3106d166777e9143e7bc5447129cb
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2018-05-15 13:43:18 +0200

    learn and forget commands

--- mando.py
+++ mando.py
@@ -3,6 +3,12 @@ import requests
 import cmd2
 import environconfig
 
+RELOAD = False
+
+class ReloadCommands(Exception):
+    """Reload the commands from server."""
+    pass
+
 
 class Config(environconfig.EnvironConfig):
     SERVER = environconfig.StringVar(default="localhost")
@@ -17,12 +23,40 @@ def create_command(name):
 
 class Mando(cmd2.Cmd):
     prompt = "mando> "
-    pass
+    @classmethod
+    def from_commands(cls, commands):
+        class _Mando(cls):
+            pass
 
-commands = requests.get(f"http://{Config.SERVER}:{Config.PORT}/getCommands").json()
+        for path in commands:
+            setattr(_Mando,
+                    f"do_{path.replace('/', '_')}",
+                    create_command(path))
+        return _Mando
 
-for path in commands:
-    name = f"do_{path.replace('/', '_')}"
-    setattr(Mando, name, create_command(path))
+    @staticmethod
+    def get_commands():
+        return requests.get(f"http://{Config.SERVER}:{Config.PORT}/getCommands").json()
 
-Mando().cmdloop(intro="Welcome to Mando!")
+    def do_learn(self, name):
+        global RELOAD
+        print(requests.get(f"http://{Config.SERVER}:{Config.PORT}/learnCommand/{name}").content)
+        RELOAD = True
+        return True
+
+    def do_forget(self, name):
+        global RELOAD
+        print(requests.get(f"http://{Config.SERVER}:{Config.PORT}/forgetCommand/{name}").content)
+        RELOAD = True
+        return True
+
+
+if __name__ == '__main__':
+    commands = Mando.get_commands()
+    mando = Mando.from_commands(commands)
+    mando().cmdloop(intro="Welcome to Mando!")
+    while RELOAD:
+        RELOAD = False
+        commands = Mando.get_commands()
+        mando = Mando.from_commands(commands)
+        mando().cmdloop(intro="Commands reloaded...")
